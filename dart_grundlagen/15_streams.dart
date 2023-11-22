09f2d42072d25cd4e7bb24d8b700d0a1f3866753
markusweiss <git@code71.de>
2023-11-22 12:49:11 +0100

    playing around with stream

--- dart_grundlagen/15_streams.dart
+++ dart_grundlagen/15_streams.dart
@@ -2,23 +2,33 @@ import 'dart:async';
 
 void main() {
   Stream<int> numberStream = NumberGenerator().getStream.asBroadcastStream();
-  StreamSubscription<int>? sub1; // Initialize it as nullable
+  StreamSubscription<int>? sub1;
+  StreamSubscription<int>? sub2;
 
   sub1 = numberStream.listen((event) {
     print("sub 1:  $event");
-    if (event >= 5) {
-      sub1!.pause(); // Use the non-nullable assertion operator (!)
+    if (event == 3) {
+      sub1!.pause();
+    }
+    if (event >= 10) {
+      sub1?.cancel();
     }
   });
 
-  StreamSubscription sub2 = numberStream.listen((event) {
+  sub2 = numberStream.listen((event) {
     print("sub 2:  $event");
+    if (event >= 7) {
+      sub1?.resume();
+    }
+    if (event >= 10) {
+      sub2?.cancel();
+    }
   });
 
   NumberGenerator numberGenerator = NumberGenerator();
   StreamSubscription<int> subscription = numberGenerator.getStream.listen(
     (counter) {
-      print(counter);
+      print("-------------");
     },
     onError: (error) {
       print("Fehler");
@@ -27,19 +37,30 @@ void main() {
       print("fertig!");
     },
   );
+
+  Future.delayed(Duration(seconds: 11), () {
+    sub1?.cancel(); // Falls noch nicht abgebrochen
+    sub2?.cancel();
+    subscription.cancel();
+    numberGenerator.getStreamController.close();
+  });
 }
 
 class NumberGenerator {
   int _counter = 0;
-
-  StreamController<int> _controller = StreamController<int>();
+  late StreamController<int> _controller;
 
   Stream<int> get getStream => _controller.stream;
+  StreamController<int> get getStreamController => _controller;
 
   NumberGenerator() {
+    _controller = StreamController<int>();
+
     Timer.periodic(Duration(seconds: 1), (timer) {
-      _controller.sink.add(_counter);
-      _counter++;
+      if (!_controller.isClosed) {
+        _controller.sink.add(_counter);
+        _counter++;
+      }
     });
   }
 }
