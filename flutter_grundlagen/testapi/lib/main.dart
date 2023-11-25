1cd6d0894eebad54b7b51b9ab08e20bd84c1771a
markusweiss <git@code71.de>
2023-11-25 18:23:15 +0100

    add stuff to test sound

--- flutter_grundlagen/testapi/lib/main.dart
+++ flutter_grundlagen/testapi/lib/main.dart
@@ -1,6 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:assets_audio_player/assets_audio_player.dart';
+
+final assetsAudioPlayer1 = AssetsAudioPlayer();
+final assetsAudioPlayer2 = AssetsAudioPlayer();
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +25,8 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    playSound();
+
     //getAdvice();
   }
 
@@ -28,42 +34,55 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Advice App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Ratschlag:',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Text(
-                  advice.isEmpty ? 'Kein Ratschlag vorhanden' : advice,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/test.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Ratschlag:',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                      fontSize: 20),
                 ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (buttonPressCount < 5) {
-                    setState(() {
-                      buttonPressCount++;
-                    });
-                  } else {
-                    getAdvice();
-                  }
-                },
-                child: Text(buttonPressCount < 5
-                    ? 'Button drücken (${5 - buttonPressCount} verbleibend)'
-                    : 'Ratschlag erhalten'),
-              ),
-            ],
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Text(
+                    advice.isEmpty ? 'Kein Ratschlag vorhanden' : advice,
+                    style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  onPressed: () {
+                    if (buttonPressCount < 5) {
+                      setState(() {
+                        buttonPressCount++;
+                      });
+                    } else {
+                      getAdvice();
+                    }
+                  },
+                  child: Text(buttonPressCount < 5
+                      ? 'Button drücken (${5 - buttonPressCount} verbleibend)'
+                      : 'Ratschlag erhalten'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -76,12 +95,28 @@ class _MyAppState extends State<MyApp> {
 
     if (response.statusCode == 200) {
       buttonPressCount = 0;
+      await assetsAudioPlayer2.open(
+        Audio("assets/sounds/native-flute-transition.mp3"),
+        autoStart: true,
+      );
+
       final Map<String, dynamic> data = json.decode(response.body);
       setState(() {
         advice = data['slip']['advice'];
       });
     } else {
       throw Exception('Failed to load advice');
+    }
+  }
+
+  void playSound() async {
+    try {
+      await assetsAudioPlayer1.open(
+          Audio("assets/sounds/restaurant-ambience.mp3"),
+          autoStart: true,
+          loopMode: LoopMode.single);
+    } catch (t) {
+      //mp3 unreachable
     }
   }
 }
